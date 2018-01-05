41e04b499d16e2c64a798b36a6c617177045f8ec
Asif-Arefin-Bonny <asifarefin92@gmail.com>
2018-01-05 10:13:53 +0600

    Design Done

---
+++ js/script.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+
+
+	$('.home-service-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true,
+			autoplay:true
+        },
+        600:{
+            items:3,
+            nav:false,
+			autoplay:true
+        },
+        1000:{
+            items:4,
+            nav:true,
+            loop:true
+        }
+    }
+});
+
+
+
+
+
+
+
+});
